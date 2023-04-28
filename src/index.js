b6613d1dc1e4fbe93d7e7fc2c6adce93084b9c4c
akyc <akyc@users.noreply.github.com>
2023-04-28 08:28:22 +0300

    wip sound mobile

--- src/index.js
+++ src/index.js
@@ -36,7 +36,8 @@ const App = () => {
                     return;
                 }
                 if (time == 1 || time == 2) {
-                    playSound('beep')
+                    //playSound('beep')
+                    s.beep.play()
                 }
                 if (time == 0) {
                     playSound('gorn')
@@ -63,8 +64,13 @@ const App = () => {
             }
         })
     }
+    let s = {}
     async function handleSubmit(e) {
         e.preventDefault()
+        s.beep = new Audio('./mp3/beep.mp3')
+        s.beep.play()
+        s.beep.pause()
+        s.beep.currentTime = 0
 
         let array = [...Array(roundsAmount).keys()]
         for (let item of array) {
@@ -74,65 +80,67 @@ const App = () => {
             }
             await roundTimer(timeConverter(roundsLength), 'Работа')
             await roundTimer(timeConverter(restLength), 'Отдых')
-
         }
     }
 
     return (
-        <div className="container">
-            <div className="row d-flex justify-content-center">
-                <div className="col-12 col-md-3">
-                    <h1 className="text-center">Tabata timer</h1>
-                    <p className="text-center">{round && `Раунд: ${round}`}</p>
-                    <div className="text-center position-relative">
-                        <svg width="200" height="200" style={{ transition: 'all 0.7s ease-in-out' }}>
-                            <circle r="70" cx="100" cy="100" fill="transparent" stroke="lightgrey" strokeWidth="2rem" strokeDasharray="439.8" strokeDashoffset="0"></circle>
-                            <circle r="70" cx="100" cy="100" style={{ transition: 'all 0.7s ease-in-out', transformOrigin: '50% 50%', transform: 'rotate(-90deg)' }} fill="transparent" strokeLinecap="round" stroke={timerType == 'Работа' ? 'red' : 'green'} strokeWidth="2rem" strokeDasharray="439.8" strokeDashoffset={439.8 - (439.8 / currentLength) * (currentLength - countDown)}></circle>
-                        </svg>
-                        <div className="position-absolute" style={{ top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}>
-                            <span className="fs-1">{countDown && `${countDown}`}</span><br />
-                            <small>{timerType && timerType}</small>
+        <>
+            <div className="container">
+                <div className="row d-flex justify-content-center">
+                    <div className="col-12 col-md-3">
+                        <h1 className="text-center">Tabata timer</h1>
+                        <p className="text-center">{round && `Раунд: ${round}`}</p>
+                        <div className="text-center position-relative">
+                            <svg width="200" height="200" style={{ transition: 'all 0.7s ease-in-out' }}>
+                                <circle r="70" cx="100" cy="100" fill="transparent" stroke="lightgrey" strokeWidth="2rem" strokeDasharray="439.8" strokeDashoffset="0"></circle>
+                                <circle r="70" cx="100" cy="100" style={{ transition: 'all 0.7s ease-in-out', transformOrigin: '50% 50%', transform: 'rotate(-90deg)' }} fill="transparent" strokeLinecap="round" stroke={timerType == 'Работа' ? 'red' : 'green'} strokeWidth="2rem" strokeDasharray="439.8" strokeDashoffset={439.8 - (439.8 / currentLength) * (currentLength - countDown)}></circle>
+                            </svg>
+                            <div className="position-absolute" style={{ top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}>
+                                <span className="fs-1">{countDown && `${countDown}`}</span><br />
+                                <small>{timerType && timerType}</small>
+                            </div>
                         </div>
+                        <form action="" onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label htmlFor="rounds_amount" className="form-label">Число раундов</label>
+                                <input
+                                    type="number"
+                                    name="rounds_amount"
+                                    onChange={(e) => setRoundsAmount(parseInt(e.target.value))}
+                                    value={roundsAmount}
+                                    className="form-control" step="1" min="1" pattern="[0-9]{2}" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="rounds_length" className="form-label">Время раунда</label>
+                                <input
+                                    type="text"
+                                    name="rounds_length"
+                                    className="form-control"
+                                    value={roundsLength}
+                                    onChange={(e) => setRoundsLength(e.target.value)}
+                                    step="1"
+                                    pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="reast_length" className="form-label">Время отдыха</label>
+                                <input
+                                    type="text"
+                                    name="rest_length"
+                                    className="form-control"
+                                    value={restLength}
+                                    onChange={(e) => setRestLength(e.target.value)}
+                                    step="1"
+                                    pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}" />
+                            </div>
+                            <div className="d-grid gap-2">
+                                {countDown ? <button type="cancel" onClick={() => location.reload()} className="btn btn-danger btn-lg">Стоп</button> : <button type="submit" className="btn btn-success btn-lg">Старт</button>}
+                            </div>
+                        </form>
                     </div>
-                    <form action="" onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <label htmlFor="rounds_amount" className="form-label">Число раундов</label>
-                            <input
-                                type="number"
-                                name="rounds_amount"
-                                onChange={(e) => setRoundsAmount(parseInt(e.target.value))}
-                                value={roundsAmount}
-                                className="form-control" step="1" min="1" pattern="[0-9]{2}" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="rounds_length" className="form-label">Время раунда</label>
-                            <input
-                                type="text"
-                                name="rounds_length"
-                                className="form-control"
-                                value={roundsLength}
-                                onChange={(e) => setRoundsLength(e.target.value)}
-                                step="1"
-                                pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="reast_length" className="form-label">Время отдыха</label>
-                            <input
-                                type="text"
-                                name="rest_length"
-                                className="form-control"
-                                value={restLength}
-                                onChange={(e) => setRestLength(e.target.value)}
-                                step="1"
-                                pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}" />
-                        </div>
-                        <div className="d-grid gap-2">
-                            {countDown ? <button type="cancel" onClick={() => location.reload()} className="btn btn-danger btn-lg">Стоп</button> : <button type="submit" className="btn btn-success btn-lg">Старт</button>}
-                        </div>
-                    </form>
                 </div>
-            </div>
-        </div >
+            </div >
+            <span style={{ position: 'absolute', right: '5px', bottom: '5px', fontSize: '26px' }} onClick={() => window.location.href = "http://азюзин.рф"}>🐗</span>
+        </>
     )
 }
 
